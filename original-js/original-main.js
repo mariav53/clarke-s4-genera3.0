ddfe9ad8461d685211edb2084121e111af68b83e
Noeliiap17 <n.parra2003@gmail.com>
2018-01-21 15:22:22 +0100

    change class to preview and footer

--- original-js/original-main.js
+++ original-js/original-main.js
@@ -171,17 +171,17 @@ inputFile.addEventListener('change', archivo);
 
 // SECCIÓN GUARDAR DATOS FORMULARIO EN PREVIEW (4FUNCIONES)
 function printPersonalDataToPreview() {
- 	document.getElementById('name-preview-id').innerHTML = document.getElementById('input-name').value;
-	document.getElementById('surname-preview-id').innerHTML = document.getElementById('input-surname').value;
-	document.getElementById('jobtitle-preview-id').innerHTML = document.getElementById('input-profession').value;
-	document.getElementById('phone-preview-id').innerHTML = document.getElementById('input-phone').value;
-	document.getElementById('birthdate-preview-id').innerHTML = document.getElementById('input-birthdate').value;
-	document.getElementById('email-preview-id').innerHTML = document.getElementById('input-email').value;
-	document.getElementById('address-preview-id').innerHTML = document.getElementById('input-address').value;
-	document.getElementById('aboutme-preview-id').innerHTML = document.getElementById('text-aboutme').value;
-	document.getElementById('twitter-preview-id').innerHTML = document.getElementById('input-twitter').value;
-	document.getElementById('linkedin-preview-id').innerHTML = document.getElementById('input-linkedin').value;
-	document.getElementById('facebook-preview-id').innerHTML = document.getElementById('input-facebook').value;
+ 	document.getElementById('personal-data--name').innerHTML = document.getElementById('input-name').value;
+	document.getElementById('personal-data--surname').innerHTML = document.getElementById('input-surname').value;
+	document.getElementById('personal-data--job').innerHTML = document.getElementById('input-profession').value;
+	document.getElementById('preview__direction--phone').innerHTML = document.getElementById('input-phone').value;
+	document.getElementById('preview__direction--date').innerHTML = document.getElementById('input-birthdate').value;
+	document.getElementById('preview__direction--mail').innerHTML = document.getElementById('input-email').value;
+	document.getElementById('preview__direction--adress').innerHTML = document.getElementById('input-address').value;
+	document.getElementById('preview__summary').innerHTML = document.getElementById('text-aboutme').value;
+	document.getElementById('preview__rrss--twitter').innerHTML = document.getElementById('input-twitter').value;
+	document.getElementById('preview__rrss--linkedin').innerHTML = document.getElementById('input-linkedin').value;
+	document.getElementById('preview__rrss--facebook').innerHTML = document.getElementById('input-facebook').value;
 }
 document.querySelector('.save-data-button').addEventListener('click',printPersonalDataToPreview);
 
