bb5a2c4cd9674f2cf094627042afa6c1754a6d5e
Noeliiap17 <n.parra2003@gmail.com>
2018-01-23 11:41:48 +0100

    change JS to ES6 in menuNav

--- original-js/nav.js
+++ original-js/nav.js
@@ -1,18 +1,23 @@
 'use strict';
 
-var navButton = document.querySelector('.nav__button')
-var navList = document.querySelector('.nav__list')
-var items = document.querySelectorAll('.nav__link');
+const navButton = document.querySelector('.nav__button')
+const navList = document.querySelector('.nav__list')
+const items = document.querySelectorAll('.nav__link');
 
-function openNav() {
+const openNav = () => {
 	navList.classList.toggle('hidden');
 }
-function closeNav() {
+
+const closeNav = () => {
 	navList.classList.add('hidden');
 }
 
-for (var i = 0; i < items.length; i++) {
-	items[i].addEventListener('click', closeNav);
+for (const itemsNAv of items) {
+	itemsNAv.addEventListener('click', closeNav);
 }
-
 navButton.addEventListener('click', openNav);
+
+
+// for (var i = 0; i < items.length; i++) {
+// 	items[i].addEventListener('click', closeNav);
+// }
