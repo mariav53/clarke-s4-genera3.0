ba38739df7372fbfd9a5fe2e32f015d380957bab
Noeliiap17 <n.parra2003@gmail.com>
2018-02-02 09:34:15 +0100

    open and close CreationDesing and create component in this seccion

---
+++ src/components/CvForm/ChoseeColors.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ButtonFont from './../../images/text.svg';
+import ButtonPalette from './../../images/paint-palette.svg';
+import ButtonTheme from './../../images/menu.svg';
+
+class ChoseeColor extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.HandleClickMenu = this.HandleClickMenu.bind(this);
+		this.state={
+			visible:false
+		}
+	}
+	HandleClickMenu(event){
+		event.preventDefault();
+		this.setState({
+			visible:!this.state.visible
+		})
+	}
+  render () {
+    return (
+			<ul className="creation__design__options">
+				<li className="creation__design__options-colour" onClick ={this.HandleClickMenu}>
+					<img className="img-design" src={ButtonPalette} title="color" alt="color" />
+					<ul className={`design-colours ${this.state.visible ? 'visible' : 'no-visible'}`}>
+						<li className="colour" data-colour-class="red-green-lime" >
+							<div className="red"></div>
+							<div className="green"></div>
+							<div className="lime"></div>
+						</li>
+						<li className="colour" data-colour-class="black-blue-lime">
+							<div className="black"></div>
+							<div className="blue"></div>
+							<div className="lime"></div>
+						</li>
+						<li className="colour" data-colour-class="black-pink-grey">
+							<div className="black"></div>
+							<div className="pink"></div>
+							<div className="grey"></div>
+						</li>
+					</ul>
+				</li>
+				</ul>
+
+    );
+  }
+}
+
+export default ChoseeColor;
---
+++ src/components/CvForm/ChoseeFont.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ButtonFont from './../../images/text.svg';
+import ButtonPalette from './../../images/paint-palette.svg';
+import ButtonTheme from './../../images/menu.svg';
+
+class ChoseeFont extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.HandleClickMenu = this.HandleClickMenu.bind(this);
+		this.state={
+			visible:false
+		}
+	}
+	HandleClickMenu(event){
+		event.preventDefault();
+		this.setState({
+			visible:!this.state.visible
+		})
+	}
+  render () {
+    return (
+			<div>
+					<ul className="creation__design__options">
+          <li className="creation__design__options-font" onClick ={this.HandleClickMenu}>
+            <img className="img-design" src={ButtonFont} title="fuente" alt="fuentes" />
+            <ul className={`design-fonts ${this.state.visible ? 'visible' : 'no-visible'}`}>
+              <li className="font" data-font-class="cookie"><h3>Cookie</h3></li>
+              <li className="font" data-font-class="annie"><h3>Annie</h3></li>
+              <li className="font" data-font-class="philosopher"><h3>Philosopher</h3></li>
+            </ul>
+          </li>
+					</ul>
+			</div>
+    );
+  }
+}
+
+export default ChoseeFont;
